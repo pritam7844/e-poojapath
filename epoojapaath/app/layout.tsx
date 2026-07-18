8358b7629077534488686c34222d91b82515f0a2
ravi2025v <ravi2025v@gmail.com>
2026-07-18 13:21:11 +0530

    feat: setup Meta Pixel and lead attribution tracking

--- epoojapaath/app/layout.tsx
+++ epoojapaath/app/layout.tsx
@@ -46,21 +46,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <Script id="fb-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            ${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID ? `fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');` : ""}
-            fbq('init', '1077299721847659');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+        {process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID && (
+          <Script id="fb-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+        )}
       </head>
       <body className="font-body antialiased">
         {process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID && (
@@ -74,15 +75,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             />
           </noscript>
         )}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1077299721847659&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
         <FacebookPixel />
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <LanguageProvider>
